cb5c32585836b9e3c96a7195cef961d09d1b6c2a
jamesgneal <nealjg@gmail.com>
2018-03-15 13:10:46 -0400

    firebase hooked up

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCyvqSCuRRwOFZwLB-Z3xGdAh4rCyC65c0",
+    authDomain: "train-scheduler-a5cbc.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a5cbc.firebaseio.com",
+    projectId: "train-scheduler-a5cbc",
+    storageBucket: "train-scheduler-a5cbc.appspot.com",
+    messagingSenderId: "473483014009"
+};
+firebase.initializeApp(config);
