95d4c76cfe75da36cbef5db0b6eadbe77c599396
Graham Neal <nealjg@gmail.com>
2018-03-19 13:16:04 -0400

    event listener for submit button captures and writes to firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,44 @@ var config = {
     storageBucket: "train-scheduler-a5cbc.appspot.com",
     messagingSenderId: "473483014009"
 };
+
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// Button for adding trains
+$("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainTime = moment($("#train-time-input").val().trim(), "HH:mm").format("X");
+    var trainFreq = $("#frequency-input").val().trim();
+  
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+      name: trainName,
+      destination: trainDest,
+      time: trainTime,
+      freq: trainFreq
+    };
+  
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+  
+    // Logs new train data to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.time);
+    console.log(newTrain.freq);
+  
+    // Alert
+    alert("Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#train-time-input").val("");
+    $("#frequency-input").val("");
+  });
